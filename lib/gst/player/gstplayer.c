51967196964f90cd451b8c16770f76ede18d2c36
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2015-06-11 13:21:45 +0200

    gir: fix introspection annotation
    gstplayer.c:3222: Error: GstPlayer: unbalanced parentheses, annotations will be ignored:
     * Returns: (transfer full) (array zero-terminated=1) (element-type

--- lib/gst/player/gstplayer.c
+++ lib/gst/player/gstplayer.c
@@ -3219,10 +3219,10 @@ gst_player_update_visualization_list (void)
 /**
  * gst_player_visualizations_get:
  *
- * Returns: (transfer full) (array zero-terminated=1) (element-type
- *  GstPlayerVisualization*): a %NULL terminated array containing all
- *  available visualizations. Use gst_player_visualizations_free()
- *  after usage.
+ * Returns: (transfer full) (array zero-terminated=1)
+ *  (element-type Gst.PlayerVisualization): a %NULL terminated
+ *  array containing all available visualizations. Use
+ *  gst_player_visualizations_free() after usage.
  *
  */
 GstPlayerVisualization **
